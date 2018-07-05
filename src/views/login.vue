c949833131f0e2fbf28b3c1c6d42367ffb4193b2
zhuguihao <zhuguihao@163.com>
2018-07-05 18:06:20 +0800

    提交新代码

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,62 +1,101 @@
 <template>
-  <div class="login">
-      <el-card class="box-card">
-        <div>
-          <img src="../assets/logo.png" alt="" class="logo">
-          <div class="margin-top">
-            账号：<el-input v-model="input" placeholder="请输入账号" maxlength="20" class="el-input" />
-          </div>
-          <div class="margin-top">
-            密码：<el-input v-model="pwd" placeholder="请输入密码" type="password" maxlength="20" class="el-input" />
-          </div>
-          <div class="margin-top">
-            <el-button type="primary" class="btn" @click="goHome">登录</el-button>
-          </div>
-        </div>
-      </el-card>
-  </div>
+  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">
+    <h3 class="title">系统登录</h3>
+    <el-form-item prop="account">
+      <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
+    </el-form-item>
+    <el-form-item prop="checkPass">
+      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
+    </el-form-item>
+    <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
+    <el-form-item style="width:100%;">
+      <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
+      <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
-export default {
-  name: 'login',
-  data () {
-    return {
-      msg: '登录界面',
-      input: '',
-      pwd: ''
-    }
-  },
-  methods: {
-    goHome () {
-      let vm = this
-      vm.$router.push({name: 'home', params: {}})
+  import { requestLogin } from '../api/api';
+  //import NProgress from 'nprogress'
+  export default {
+    data() {
+      return {
+        logining: false,
+        ruleForm2: {
+          account: 'admin',
+          checkPass: '123456'
+        },
+        rules2: {
+          account: [
+            { required: true, message: '请输入账号', trigger: 'blur' },
+            //{ validator: validaePass }
+          ],
+          checkPass: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            //{ validator: validaePass2 }
+          ]
+        },
+        checked: true
+      };
+    },
+    methods: {
+      handleReset2() {
+        this.$refs.ruleForm2.resetFields();
+      },
+      handleSubmit2(ev) {
+        var _this = this;
+        this.$refs.ruleForm2.validate((valid) => {
+          if (valid) {
+            //_this.$router.replace('/table');
+            this.logining = true;
+            //NProgress.start();
+            var loginParams = { username: this.ruleForm2.account, password: this.ruleForm2.checkPass };
+            requestLogin(loginParams).then(data => {
+              this.logining = false;
+              //NProgress.done();
+              let { msg, code, user } = data;
+              if (code !== 200) {
+                this.$message({
+                  message: msg,
+                  type: 'error'
+                });
+              } else {
+                sessionStorage.setItem('user', JSON.stringify(user));
+                this.$router.push({ path: '/table' });
+              }
+            });
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      }
     }
   }
-}
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  .logo{
-    width: 10rem;
-    height: 10rem;
-  }
-  .el-input{
-    width: 20rem;
-    /*height: 1.5rem;*/
-  }
-  .margin-top{
-    margin-top: 1.5rem;
-  }
-  .box-card{
-    width: 50rem;
-    text-align: center;
-    position: fixed;
-    left: 25%;
-    top: 25%;
-  }
-  .btn{
-    width: 10rem;
+<style lang="scss" scoped>
+  .login-container {
+    /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+    background-clip: padding-box;
+    margin: 180px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+    .title {
+      margin: 0px auto 40px auto;
+      text-align: center;
+      color: #505458;
+    }
+    .remember {
+      margin: 0px 0px 35px 0px;
+    }
   }
 </style>
